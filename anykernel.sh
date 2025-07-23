f3d0557dca6f54de69ff6a56575ebdb288d50d22
Xavier Libres Añora <xavierlibresanora@gmail.com>
2025-07-23 14:32:31 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ブルーアーカイブ-Kernel-RC6
+kernel.string=ブルーアーカイブ-KERNELSUNEXT-RC7
 do.devicecheck=1
 do.modules=0
 do.systemless=1
